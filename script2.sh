6ac5eb0ae9ccdb3b15b292e8ede4b760e6e0888d
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2025-12-03 21:05:16 +0000

    arquitectura

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
-uname -a #uname es un comando que nos sirve para mostrar informacion basica del sistema mientras que -anos sirve para mostrar toda la informacion posible por asi decirlo mostrar toda la informacion que se pueda.
-lscpu
-free -h
-df -h
+uname -a #uname es un comando que nos sirve para mostrar informacion basica del sistema mientras que -a nos sirve para mostrar toda la informacion posible por asi decirlo mostrar toda la informacion que se pueda.
+lscpu #este comando sirve para listar por asi decirlo informacion detallada del procesador
+free -h #Free nos sirve para ver el estado de memoria RAM y la swap y la funcion de -h es para que sea legible para humanos
+df -h #El comando df sirve para mostrar cuanto espacio de disco esta usado y lo que queda libre  y como antes el -h es para que sea legible para humanos.
