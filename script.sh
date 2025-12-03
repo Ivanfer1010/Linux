470a6c4e58357cdbe6f1728e6392dd3f553579ae
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2025-12-03 15:42:56 +0000

    trabajo arquitectura

---
+++ script.sh
@@ -0,0 +1,5 @@
+echo "Hola linux" > archivo.txt #se usa para mostrar o imprimir el texto en la terminal debe ser escrito lo que queremos entre comillas y > nos ayuda a redireccionar el texto al archivo que pusimos despues en mi caso el archivo.txt
+cat archivo.txt #
+cp archivo.txt copia.txt
+ls -l
+rm archivo.txt
