5ac9082005f0911862f0e44ab0583aba4f06c521
Ivanfer1010 <ivgualotunabo@uide.edu.ec>
2025-12-03 16:00:31 +0000

    arquitectura

--- script2.sh
+++ script2.sh
@@ -1,4 +1,4 @@
-uname -a #
+uname -a #uname es un comando que nos sirve para mostrar informacion basica del sistema mientras que -anos sirve para mostrar toda la informacion posible por asi decirlo mostrar toda la informacion que se pueda.
 lscpu
 free -h
 df -h
